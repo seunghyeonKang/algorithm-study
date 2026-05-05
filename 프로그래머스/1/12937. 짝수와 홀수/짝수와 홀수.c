b61648a6c29ff01288c5e0a79e2e915fb646417d
강승현 <101852433+seunghyeonKang@users.noreply.github.com>
2026-05-05 12:05:01 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* solution(int num) {
+    // 리턴할 값은 메모리를 동적 할당해주세요
+    char* answer = num % 2 ? "Odd" : "Even";
+    return answer;
+}
